0ac5e461837e83d996bc60959b9a30338f3e3a82
Alexey Mekhanoshin <367257+Ales999@users.noreply.github.com>
2025-07-12 01:36:03 +0500

    Correct close server with Ctrl-C and add check valid port number

--- Server/main.go
+++ Server/main.go
@@ -1,55 +1,108 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"syscall"
 	"time"
+	"unicode"
 )
 
 func PrintUsage() {
-	fmt.Println("Usage example:")
-	fmt.Printf("%s 1457\n", os.Args[0])
+	fmt.Println("Пример использования:")
+	fmt.Printf("%s <port>\n", os.Args[0])
 }
 
+var wg sync.WaitGroup
+
 func main() {
-
-	// Порт который будем слушать
-	var port string
-
-	if len(os.Args) > 1 {
-		port = os.Args[1]
-	} else {
+	if len(os.Args) < 2 {
+		PrintUsage()
+		return
+	}
+	// Проверим корректность номера порта
+	port, valid := validatePort(os.Args[1])
+	if !valid {
+		fmt.Printf("Ошибка: '%s' - не валидный порт (должно быть число от 1 до 65535)\n", os.Args[1])
 		PrintUsage()
 		return
 	}
 
-	// listen to incoming udp packets
-	udpServer, err := net.ListenPacket("udp", ":"+port)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	udpServer, err := net.ListenPacket("udp4", ":"+port)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer udpServer.Close()
 
-	fmt.Println("Sever started and wait UDP packet to this port:", port)
+	go func(conn net.PacketConn) {
+		<-sigChan
+		fmt.Println("\nОстанавливаем сервер...")
+		cancel()
+		udpServer.Close() // Закрываем соединение перед остановкой сервера
+	}(udpServer)
 
-	for {
-		buf := make([]byte, 1024)
+	fmt.Println("Сервер запущен и ожидает UDP-пакеты на порту:", port)
+
+	buf := make([]byte, 1024)
+
+	for ctx.Err() == nil {
 		n, addr, err := udpServer.ReadFrom(buf)
 		if err != nil {
+			if ctx.Err() != nil {
+				break
+			}
+			log.Println("Ошибка чтения из UDP:", err, "IP:", addr.String())
 			continue
 		}
-		fmt.Printf("packet-received: bytes=%d from=%s\n", n, addr.String())
 
-		go response(udpServer, addr, buf[:n])
+		fmt.Printf("packet-received: bytes=%d from=%s\n", n, addr.String())
+		wg.Add(1) // Инкрементируем WaitGroup перед запуском новой горутины
+		go func(ctx context.Context, conn net.PacketConn, addr net.Addr, msg []byte) {
+			defer wg.Done() // Декриментируем WaitGroup в конце горутины
+			response(ctx, conn, addr, msg)
+		}(ctx, udpServer, addr, buf[:n])
 	}
 
+	// Ждём завершения всех горутин перед закрытием сервера
+	wg.Wait()
 }
 
-func response(udpServer net.PacketConn, addr net.Addr, buf []byte) {
-	time := time.Now().Format(time.ANSIC)
+func response(ctx context.Context, conn net.PacketConn, addr net.Addr, msg []byte) {
+	select {
+	case <-ctx.Done():
+		return
+	default:
+		timeStr := time.Now().Format(time.ANSIC)
+		response := fmt.Sprintf("time received: %v. Your message: %v!", timeStr, string(msg))
+		if _, err := conn.WriteTo([]byte(response), addr); err != nil {
+			log.Println("Ошибка записи в UDP:", err)
+		}
+	}
+}
 
-	responseStr := fmt.Sprintf("time received: %v. Your message: %v!", time, string(buf))
-	udpServer.WriteTo([]byte(responseStr), addr)
+func validatePort(portStr string) (string, bool) {
+	portInt, err := strconv.Atoi(portStr)
+	if err != nil || portInt < 1 || portInt > 65535 {
+		return "0", false
+	}
+
+	// Check that all characters are digits
+	for _, c := range portStr {
+		if !unicode.IsDigit(c) {
+			return "0", false
+		}
+	}
+	return strconv.Itoa(portInt), true
 }
